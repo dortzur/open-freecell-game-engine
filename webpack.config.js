e81c4eaa44a8c125f8f489d1b5e83f30123adce8
Dor Tzur <dortzur@gmail.com>
2016-02-12 23:18:10 +0200

    de-frontend

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,14 @@ var path = require('path');
 
 module.exports = {
     entry: [
-    'webpack-dev-server/client?http://0.0.0.0:8080', // WebpackDevServer host and port
-    'webpack/hot/only-dev-server',
-    './index.jsx' // Your appʼs entry point
-  ],
+        'webpack-dev-server/client?http://0.0.0.0:8080', // WebpackDevServer host and port
+        'webpack/hot/only-dev-server',
+        './index.js' // Your appʼs entry point
+    ],
     devtool: process.env.WEBPACK_DEVTOOL || 'source-map',
     output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
+        library: "FreecellGameEngine",
+        libraryTarget:"umd"
     },
     resolve: {
         extensions: ['', '.js', '.jsx']
@@ -18,45 +18,11 @@ module.exports = {
     module: {
         loaders: [
             {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loaders: ['react-hot', 'babel'],
-            },
-
-            {
-                test: /\.css$/,
-                loader: 'style-loader!css-loader'
-            },
-            {
-                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "file"
-            },
-            {
-                test: /\.(woff|woff2)$/,
-                loader: "url?prefix=font/&limit=5000"
-            },
-            {
-                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url?limit=10000&mimetype=application/octet-stream"
-            },
-            {
-                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url?limit=10000&mimetype=image/svg+xml"
-            },
-            {
-                test: /\.gif/,
-                loader: "url-loader?limit=10000&mimetype=image/gif"
-            },
-            {
-                test: /\.jpg/,
-                loader: "url-loader?limit=10000&mimetype=image/jpg"
-            },
-            {
-                test: /\.png/,
-                loader: "url-loader?limit=10000&mimetype=image/png"
+                test: /\.js$/,
+                loaders: ['babel-loader'],
+                exclude: /node_modules/
             }
-
-             ]
+        ]
     },
     devServer: {
         contentBase: "./public",
@@ -65,6 +31,6 @@ module.exports = {
         inline: true
     },
     plugins: [
-      new webpack.NoErrorsPlugin()
+        new webpack.NoErrorsPlugin()
     ]
 };
