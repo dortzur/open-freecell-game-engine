c37db3074eabdd9aec087c75011cd1f31d9578c6
Dor Tzur <dortzur@gmail.com>
2016-02-18 23:37:15 +0200

    test suits added

---
+++ test/misc.spec.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+const GameEngine = require('../src');
+
+describe('Miscellaneous tests', () => {
+    it('validate ...', () => {
+    })
+});
---
+++ test/validationActionsCoco.spec.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+const GameEngine = require('../src');
+
+describe('Extended tests for COCO actions', () => {
+    it('validate ...', () => {
+    })
+});
